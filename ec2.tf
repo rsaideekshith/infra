5cd0926ffafb002e79e5613c69c0f65ea3fa1af1
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-07 17:07:46 +0530

    edited more

--- ec2.tf
+++ ec2.tf
@@ -6,14 +6,13 @@ module "myec2_calling_module" {
 
     ami_id = data.aws_ami.ubuntu.id
     # ami_id = var.ami_id
-    instance_type = var.instance_type
+
     project = "MY_TF_PRO"
     
-     key_name = module.my_keypair_calling_module.key_name
 
     public_subnet_id = module.my_public_subnet_calling_module.my_public_subnet_id
 
-    vpc_security_group_ids = module.my_sg_calling_module.security_group_id
+    
 
 
 
