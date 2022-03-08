a963953cbe06cebc46a3cd8f315ff50918e2977c
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-08 18:31:06 +0530

    changes finished

--- sg.tf
+++ sg.tf
@@ -2,4 +2,5 @@ module "my_sg_calling_module" {
   source = "github.com/rsaideekshith/terraform-modules/sg-module"
 
   vpc_id      =  module.myvpc_calling_module.myvpc_id
+  public_subnet_cidr_block = module.my_public_subnet_calling_module.my_public_subnet_cidr_block
 }
