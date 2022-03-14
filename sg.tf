a9f3e9eb20dcc752272fc0c23fc73f3848b09983
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-14 17:09:03 +0530

    new mode

--- sg.tf
+++ sg.tf
@@ -1,7 +1,5 @@
 module "my_sg_calling_module" {
   source = "github.com/rsaideekshith/terraform-modules/sg-module"
-  name = "my_security_group"
   vpc_id = module.my_sg_calling_module.vpc_id
-   cidr_blocks      =  ["10.20.0.0/25"]
 
 }
