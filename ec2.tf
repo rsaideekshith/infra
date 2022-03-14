6c925cd41e0512e1ec29e3479df1a99adffab043
Sai Deekshith <rsaideekshith@gmail.com>
2022-03-14 17:44:47 +0530

    new mode

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,8 @@ module "myec2_calling_module"  {
     instance_count = var.instance_count
     ami_id = data.aws_ami.ubuntu.id
     # ami_id = var.ami_id
-
+subnet_id = module.my_subnet.subnet_id
+vpc_security_group_id = module.vpc_security_group_id.aws_security_group.allow_ssh.id
      
        
      
